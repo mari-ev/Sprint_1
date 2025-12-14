8b67c358c7bbdd76a8dfcac9abafc9c45602c0d5
Maria Evdoshenko <evdoshenkomaria@yandex.ru>
2025-12-14 11:25:54 +0300

    First commit task 1-5

--- task_4.py
+++ task_4.py
@@ -1,8 +1,8 @@
-new_tasks = ['task_001', 'task_011', 'task_007', 'task_015', 'task_005']
-completed_tasks = ['task_002', 'task_012', 'task_006']
+new_tasks = ["task_001", "task_011", "task_007", "task_015", "task_005"]
+completed_tasks = ["task_002", "task_012", "task_006"]
 
 completed_tasks.append(new_tasks.pop(-1))
 
 last_task = new_tasks.pop()
 
-print(last_task) 
+print(last_task)
